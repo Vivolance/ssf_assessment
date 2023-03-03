b18d1f4ba392767c35ce4def35966521c555fc0f
elsonchan <elsonccj@gmail.com>
2023-03-03 09:23:19 +0800

    First Commit

---
+++ src/test/java/Batch02/SSF/Assessment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Batch02.SSF.Assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
